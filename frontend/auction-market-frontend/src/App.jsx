c7a1bb70831a92c220b3be8618f14f8aaa871f65
mohitthakur007 <thakur.mohitkumar007@gmail.com>
2024-05-08 00:06:14 +0530

    frontend setup basics

---
+++ frontend/auction-market-frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+function App() {
+  return (
+    <>
+    <label>Username</label>
+    <input type="text" placeholder='enter username'></input>
+    <br></br>
+    <br></br>
+    <label>Password</label>
+    <input type="password" placeholder='enter password'></input>
+    <br></br>
+    <br></br>
+    
+
+    <button>Login</button>
+</>
+    
+  )
+}
+
+export default App
